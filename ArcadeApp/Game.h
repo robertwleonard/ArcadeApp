cb3e493dec9b500418a5c2c8b2cad83e1e3f9a42
Robert W. Leonard <robert@draknor.com>
2019-03-26 18:42:38 -0500

    Set up game objects

---
+++ ArcadeApp/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <stdint.h>
+
+class GameController;
+class Screen;
+
+class Game
+{
+public:
+	virtual ~Game();
+	virtual void Init(GameController& controller) = 0;
+	virtual void Update(uint32_t dt) = 0;
+	virtual void Draw(Screen& screen) = 0;
+	virtual std::string GetName() const = 0;
+};
+
