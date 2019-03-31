6ee9b6889bc2a3cad868bb4adbe8ab4a875905d9
Robert W. Leonard <robert@draknor.com>
2019-03-30 21:08:48 -0500

    Start main options scene and buttons

---
+++ ArcadeApp/Button.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include "AARectangle.h"
+#include <functional>
+#include "Color.h"
+#include "BitmapFont.h"
+#include "InputAction.h"
+
+class Screen;
+
+class Button
+{
+public:
+	using ButtonAction = std::function<void(void)>;
+	Button(const BitmapFont& bitmapFont, const Color& textColor, const Color& highlightColor = Color::White());
+	void Init(Vec2D topLeft, unsigned int width, unsigned int height);
+	void Draw(Screen& theScreen);
+	void ExecuteAction();
+
+	inline void SetButtonText(const std::string& text) { mTitle = text; }
+	inline const std::string& GetButtonText() const { return mTitle; }
+	inline void SetHighlighted(bool highlighted) { mHighlighted = highlighted; }
+	inline bool IsHighlighted() const { return mHighlighted; }
+	inline void SetButtonAction(Button::ButtonAction action) { mAction = action; }
+
+private:
+	const BitmapFont& mBitmapFont;
+	std::string mTitle;
+	AARectangle mBBox;
+
+	bool mHighlighted;
+	ButtonAction mAction;
+	Color mHighlightColor;
+	Color mTextColor;
+};
+
